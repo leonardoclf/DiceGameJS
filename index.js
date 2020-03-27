4631d90ae9005d65658f5e02e02312c6701cfbf3
leonardoclf <leonardoclf@gmail.com>
2020-03-27 13:04:52 -0300

    v1

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor((Math.random() * 6)) + 1;
+var setImg1 = "images/dice" + randomNumber1 +".png";
+var randomNumber2 = Math.floor((Math.random() * 6)) + 1;
+var setImg2 = "images/dice" + randomNumber2 +".png";
+
+document.querySelector(".img1").setAttribute("src", setImg1);
+document.querySelector(".img2").setAttribute("src", setImg2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+} else if (randomNumber2 > randomNumber1 ) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
+
+
